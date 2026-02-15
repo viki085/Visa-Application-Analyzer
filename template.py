f77f6c7d5bd7ec0c813d3d472eddcc548a38b4b5
Vikram <viki.kma085@gmail.com>
2026-02-16 00:30:50 +0530

    Project Structure Created

---
+++ template.py
@@ -0,0 +1,57 @@
+import os
+from pathlib import Path 
+
+project_name = "visa-app"
+
+list_of_files = [
+    f"{project_name}/__init__.py",
+    f"{project_name}/components/__init__.py",
+    f"{project_name}/components/data_ingestion.py",
+    f"{project_name}/components/data_validation.py",
+    f"{project_name}/components/data_transformation.py",
+    f"{project_name}/components/model_trainer.py",
+    f"{project_name}/components/model_evaluation.py",
+    f"{project_name}/components/model_pusher.py",
+    f"{project_name}/configuration/__init__.py",
+    f"{project_name}/constatnts/__init__.py",
+    f"{project_name}/entity/__init__.py",
+    f"{project_name}/entity/artifact_entity.py",
+    f"{project_name}/entity/config_entity.py",
+    f"{project_name}/exception/__init__.py",
+    f"{project_name}/logger/__init__.py",
+    f"{project_name}/pipeline/__init__.py",
+    f"{project_name}/pipeline/training_pipeline.py",
+    f"{project_name}/pipeline/prediction_pipeline.py",
+    f"{project_name}/utils/__init__.py",
+    f"{project_name}/utils/main_utils.py",
+    "app.py",
+    "Dockerfile",
+    ".dockerignore",
+    "setup.py",
+    "demo.py",
+    "config/model.yaml",
+    "config/schema.yaml"
+]
+
+for file in list_of_files:
+    file_path = Path(file)
+    file_dir, file_name = os.path.split(file_path)
+
+    if file_dir != "":
+        os.makedirs(file_dir, exist_ok=True)
+        print(f"Directory '{file_dir}' created successfully.")
+    
+    if not os.path.exists(file_path) or os.path.getsize(file_path) == 0:
+        with open(file_path, "w") as f:
+            pass
+        print(f"File '{file_name}' created successfully.")
+    else:
+        print(f"File '{file_name}' already exists.")
+
+
+
+
+
+
+
+
