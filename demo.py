8a8318972db5d55f42e8e9168813d887576ed79a
Vikram <viki.kma085@gmail.com>
2026-02-16 17:02:39 +0530

    Created Data Ingestion

--- demo.py
+++ demo.py
@@ -1,11 +1,5 @@
-import sys
-from visa.logger import logging
-from visa.exception import VisaException
+from visa.pipeline.training_pipeline import TrainingPipeline 
 
-# logging.info("This is a test log message from demo.py")
 
-try:
-    a = 1 / 0
-except Exception as e:
-    logging.info("An error occurred in demo.py")
-    raise VisaException(e, sys)
+pipeline = TrainingPipeline()
+pipeline.run_pipeline()
---
+++ visa/components/data_ingestion.py
@@ -0,0 +1,98 @@
+import os
+import sys
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from visa.entity.config_entity import DataIngestionConfig
+from visa.entity.artifact_entity import DataIngestionArtifact
+from visa.exception import VisaException
+from visa.logger import logging
+from visa.data_access.visa_data import VisaData
+
+
+class DataIngestion:
+    def __init__(self,data_ingestion_config:DataIngestionConfig=DataIngestionConfig()):
+        """
+
+        """
+        try:
+            self.data_ingestion_config = data_ingestion_config
+        except Exception as e:
+            raise VisaException(e,sys)
+        
+    
+    def export_data_into_feature_store(self)->pd.DataFrame:
+        """
+ 
+        """
+        try:
+            logging.info(f"Exporting data from mongodb")
+            usvisa_data = VisaData()
+            dataframe = usvisa_data.export_collection_as_dataframe(collection_name=
+                                                                   self.data_ingestion_config.collection_name)
+            logging.info(f"Shape of dataframe: {dataframe.shape}")
+            feature_store_file_path  = self.data_ingestion_config.feature_store_file_path
+            dir_path = os.path.dirname(feature_store_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            logging.info(f"Saving exported data into feature store file path: {feature_store_file_path}")
+            dataframe.to_csv(feature_store_file_path,index=False,header=True)
+            return dataframe
+
+        except Exception as e:
+            raise VisaException(e,sys)
+        
+    
+
+    def split_data_as_train_test(self,dataframe: pd.DataFrame) ->None:
+        """
+ 
+        """
+        logging.info("Entered split_data_as_train_test method of Data_Ingestion class")
+
+        try:
+            train_set, test_set = train_test_split(dataframe, test_size=self.data_ingestion_config.train_test_split_ratio)
+            logging.info("Performed train test split on the dataframe")
+            logging.info(
+                "Exited split_data_as_train_test method of Data_Ingestion class"
+            )
+            dir_path = os.path.dirname(self.data_ingestion_config.training_file_path)
+            os.makedirs(dir_path,exist_ok=True)
+            
+            logging.info(f"Exporting train and test file path.")
+            train_set.to_csv(self.data_ingestion_config.training_file_path,index=False,header=True)
+            test_set.to_csv(self.data_ingestion_config.testing_file_path,index=False,header=True)
+
+            logging.info(f"Exported train and test file path.")
+        except Exception as e:
+            raise VisaException(e, sys) from e
+        
+    
+
+    def initiate_data_ingestion(self) ->DataIngestionArtifact:
+        """
+    
+        """
+        logging.info("Entered initiate_data_ingestion method of Data_Ingestion class")
+
+        try:
+            dataframe = self.export_data_into_feature_store()
+
+            logging.info("Got the data from mongodb")
+
+            self.split_data_as_train_test(dataframe)
+
+            logging.info("Performed train test split on the dataset")
+
+            logging.info(
+                "Exited initiate_data_ingestion method of Data_Ingestion class"
+            )
+
+            data_ingestion_artifact = DataIngestionArtifact(trained_file_path=self.data_ingestion_config.training_file_path,
+            test_file_path=self.data_ingestion_config.testing_file_path)
+            
+            logging.info(f"Data ingestion artifact: {data_ingestion_artifact}")
+            return data_ingestion_artifact
+        except Exception as e:
+            raise VisaException(e, sys) from e
+
+
+
